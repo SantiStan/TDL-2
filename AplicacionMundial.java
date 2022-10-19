366e60453f2ff8ea69b4faac979fd604aabb19cc
SantiStan <116046878+SantiStan@users.noreply.github.com>
2022-10-19 01:07:51 -0300

    Update AplicacionMundial.java

--- AplicacionMundial.java
+++ AplicacionMundial.java
@@ -126,29 +126,53 @@ public class main {
 // busca un pais en la base de datos, y en caso de no encontrarla, este lo agrega
 		
 	public static void AgregarJugador(Connection con){	
-		System.out.println("Escriba el nombre del pais que quiere agregar: ");
+		System.out.println("Ingresar apellido de futbolista: ");
 		String apellido = input.nextLine();
-		if(apellido == null) {
-			try {
-				System.out.print("Escriba el idioma del pais: ");
-				String nombre = input.nextLine();
-				String docIdentidad = input.nextLine();
-				String telefono = input.nextLine();
-				String email = input.nextLine();
-				
-				String query = "“INSERT INTO jugadores values (?,?,?,?,?,?)";
-				PreparedStatement st = con.prepareStatement(query);
-				st.setString(1, nombre);
-				st.setString(2, apellido);
-				st.setString(3, docIdentidad);
-				st.setString(4, telefono);
-				st.setString(5, email);
-				System.out.println("Agregado exitosamente");
-			}catch(SQLException e) {        
-				System.out.println(e.getMessage());
-			}
-		}
-		else System.out.print("El pais que esta intentanto ingresar ya existe");
+		if (BuscarJugador(apellido,con)!=null) System.out.println("El futbolista ingresado ya existe.");
+		else {
+			System.out.println("Ingresar nombre: ");
+			String nombre = input.nextLine();
+			System.out.println("Ingresar telefono: ");
+			int telefono = input.nextInt();
+			System.out.println("Ingresar numero de documento: ");
+			int docIdentidad = input.nextInt();
+			System.out.println("Ingresar email: ");
+			String mail = input.nextLine();
+				try{
+					String query = "SELECT idpais,nombre FROM pais";
+					Statement st = con.createStatement();
+					ResultSet rs= st.executeQuery(query);
+					System.out.println("Escriba el nombre de id del pais que representa el jugador: ");
+					while (rs.next()){
+						System.out.println(rs.getInt("idpais")+" : "+rs.getString("nombre"));
+					}
+					int paisID = sc.nextInt();
+					rs= st.executeQuery("SELECT nombre FROM pais WHERE idpais = "+paisID);
+					System.out.println("¿Quiere agregar a "+nombre+" "+apellido+" a "+rs.getString("pais")+"? SI / NO");
+					String opcion = sc.nextLine();
+					boolean valido = false;
+					while (!valido) {
+						if (opcion.equals("SI")) {
+							valido = true;
+							// INSERTAR DATOS
+							st.executeUpdate("INSERT INTO futbolista (nombre,apellido,docIdentidad,telefono,mail,idpais) VALUES('"+nombre+"','"+apellido+"','"+docIdentidad+"','"+telefono+"','"+mail+"','"+paisID+"')");
+							System.out.println("Agregado exitosamente");
+						} else {
+							if (opcion.equals("NO")) {
+								valido = true;
+							} 
+							else {
+								System.out.println("Invalido");
+								System.out.println("¿Quiere agregar a "+nombre+" "+apellido+" en "+rs.getString("pais")+"? SI / NO");
+								opcion = sc.nextLine();
+							}
+						}
+					}
+					rs.close();
+					st.close();
+					} catch (java.sql.SQLException e) {
+						System.out.println("Error de SQL: "+e.getMessage());
+					}
 	}
 
 // Busca detro de la base de datos el pais que se pide.
@@ -173,29 +197,65 @@ public class main {
 		
 //busca una sede en la base de datos, y en caso de no encontrarla, este lo agrega
 		public static void AgregarSede(Connection con){				
-			try {
-				
-			}catch(SQLException e) {        
-				System.out.println(e.getMessage());
-			}
+			System.out.println("Ingresar sede: ");
+			String nombre = input.nextLine();
+			if (BuscarSede(nombre,con)!=null){
+				try{
+					Statement st = con.createStatement();
+					System.out.println("Ingresar capacidad: ");
+					int capacidad = input.nextInt();
+					System.out.println("Ingresar pais: ");
+					String pais = input.nextLine();
+					ResultSet rs= st.executeQuery("SELECT * FROM pais WHERE nombre = "+pais);
+					int res=st.executeUpdate("INSERT INTO sede (nombre,capacidad,idpais) VALUES('"+nombre+"','"+capacidad+"','"+rs.getInt("idpais")+"')");
+					if (res==0) System.out.println("Agregado exitosamente");
+					else System.out.println("Ocurrio un error");
+					st.close();
+				 } catch (java.sql.SQLException e) {
+					System.out.println("Error de SQL: "+e.getMessage());
+				 }
+			else System.out.println("La sede ingresado ya existe.");	
 		}
 		
 // busca una sede y la edita
 		public static void EditarSede(Connection con) {
-			try {
-				
-			}catch(SQLException e) {        
-				System.out.println(e.getMessage());
-			}
+			System.out.println("Ingresar sede: ");
+			String nombre = input.nextLine();
+				try{
+					if (BuscarSede(nombre,con)==null) System.out.println("La sede ingresada no existe.");
+					else {
+						Statement st = con.createStatement();	
+						System.out.println("Ingresar capacidad: ");
+						int capacidad = input.nextInt();
+						System.out.println("Ingresar pais: ");
+						String pais = input.nextLine();
+						ResultSet rs= st.executeQuery("SELECT * FROM pais WHERE nombre = "+pais);
+						int res=st.executeUpdate("UPDATE sede (nombre,capacidad,idpais) VALUES('"+nombre+"','"+capacidad+"','"+rs.getInt("idpais")+"') where nombre ="+nombre);
+						if (res==0) System.out.println("Editado exitosamente");
+						else System.out.println("Ocurrio un error");
+						rs.close();
+						st.close();
+					}
+				 } catch (java.sql.SQLException e) {
+					 System.out.println("Error de SQL: "+e.getMessage());
+				 }
 		}
 // busca una sede y la borra
 		
 		public static void EliminarSede(Connection con) {
-			try {
-				
-			}catch(SQLException e) {        
-				System.out.println(e.getMessage());
-			}
+			System.out.println("Ingresar sede: ");
+			String nombre = input.nextLine();
+				try{
+					if (BuscarSede(nombre,con)!=null) {
+						Statement st = con.createStatement();	
+						st.executeUpdate("DELETE FROM sede WHERE nombre = "+nombre);
+						System.out.println("Eliminado exitosamente");
+						st.close();
+					}
+					else System.out.println("La sede ingresada no existe.");
+				 } catch (java.sql.SQLException e) {
+					 System.out.println("Error de SQL: "+e.getMessage());
+				 }
 		}
 		
 		
