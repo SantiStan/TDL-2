c1364a7775a3e4e65c89885c5dae75aaab84dac3
SantiStan <116046878+SantiStan@users.noreply.github.com>
2022-10-19 18:02:36 -0300

    Update AplicacionMundial.java

--- AplicacionMundial.java
+++ AplicacionMundial.java
@@ -1,7 +1,7 @@
 package PrototiposClases;
 
+
 import java.sql.*;
-import javax.sql.*;
 import java.util.Scanner;
 
 public class main {
@@ -14,18 +14,17 @@ public class main {
 		
 		String url = "jdbc:mysql://localhost:3306/mundial_futbol_2022";
 		
+		Connection con = null;
+		
 		try {
-			Connection con = DriverManager.getConnection(url,"root","Santutu");
-				
-			int selection = 0;
-
-		
-			System.out.println("Opción 1: Agregar pais.\nOpción 2: Agregar jugador.\nOpción 3: Agregar sede.\nOpción 4: Edicion sede.\nOpción 5: Eliminacion sede.");
-		
-			while (selection<6){
-					
+			con = DriverManager.getConnection(url,"root","Santutu");
 			
-				selection = input.nextInt();
+
+			System.out.println("Elija una opcion:\n-Opción 0: Salir del sistema.\n-Opción 1: Agregar pais.\n-Opción 2: Agregar jugador.\n-Opción 3: Agregar sede.\n-Opción 4: Edicion sede.\n-Opción 5: Eliminacion sede.");
+		
+			int selection = input.nextInt();
+			
+			while (selection!=0){
 			
 				switch(selection){
 	        		case 1:  		
@@ -47,12 +46,17 @@ public class main {
 	        		case 5:
 	        			EliminarSede(con);
 	        			break;
+	        			
+	        		default: 
+	        			System.out.println("Error: Opcion invalida.");
 	             
 				}	
 				
 				System.out.println("Opción 1: Agregar pais.\nOpción 2: Agregar jugador.\nOpción 3: Agregar sede.\nOpción 4: Edicion sede.\nOpción 5: Eliminacion sede.");			
-	     }
-
+				selection = input.nextInt();
+		}
+		
+		System.out.print("Usted salio del menu, hasta luego.");
 		
 	}catch(SQLException e) {        
 		System.out.println(e.getMessage());
@@ -85,22 +89,26 @@ public class main {
 	
 // Busca un pais en la base de datos, y en caso de no encontrarla, este lo agrega
 	public static void AgregarPais(Connection con){				
-		System.out.println("Escriba el nombre del pais que quiere agregar: ");
+		System.out.println("Ingresar el nombre del pais: ");
+		input.nextLine();
 		String nombre = input.nextLine();
-		if(nombre == null) {
-			try {
-				System.out.print("Escriba el idioma del pais: ");
+		if (BuscarPais(nombre,con)!=null) {
+			try{
+				System.out.println("Ingresar idioma: ");
 				String idioma = input.nextLine();
-				String query = "“INSERT INTO pais values (?,?)";
-				PreparedStatement st = con.prepareStatement(query);
-				st.setString(1, nombre);
-				st.setString(2, idioma);
-				System.out.println("Agregado exitosamente");
-			}catch(SQLException e) {        
+				String query = "INSERT INTO pais (nombre,idioma) VALUES( ? , ? )";
+				PreparedStatement pst = con.prepareStatement(query);
+				pst.clearParameters();
+				pst.setString(1,nombre);
+				pst.setString(2,idioma);
+				pst.executeUpdate();
+				System.out.println("Se agrego exitosamente el pais");
+				pst.close();
+			} catch (java.sql.SQLException e) {
 				System.out.println(e.getMessage());
 			}
 		}
-		else System.out.print("El pais que esta intentanto ingresar ya existe");		
+		else System.out.println("El pais ingresado ya existe.");		
 	}
 	
 //Busca detro de la base de datos el pais que se pide.
@@ -126,54 +134,71 @@ public class main {
 // busca un pais en la base de datos, y en caso de no encontrarla, este lo agrega
 		
 	public static void AgregarJugador(Connection con){	
-		System.out.println("Ingresar apellido de futbolista: ");
+		input.nextLine();
+		System.out.println("Ingresar apellido del jugador: ");
+		String nombre = input.nextLine();
+		System.out.println("Ingresar el nombre: ");
 		String apellido = input.nextLine();
-		if (BuscarJugador(apellido,con)!=null) System.out.println("El futbolista ingresado ya existe.");
+		if (BuscarFutbolista(apellido,con)!=null) System.out.println("El futbolista ingresado ya existe.");
 		else {
-			System.out.println("Ingresar nombre: ");
-			String nombre = input.nextLine();
 			System.out.println("Ingresar telefono: ");
 			int telefono = input.nextInt();
-			System.out.println("Ingresar numero de documento: ");
+			input.nextLine();
+			System.out.println("Ingresar documento de identidad: ");
 			int docIdentidad = input.nextInt();
-			System.out.println("Ingresar email: ");
+			input.nextLine();
+			System.out.println("Ingresar mail: ");
 			String mail = input.nextLine();
 				try{
 					String query = "SELECT idpais,nombre FROM pais";
 					Statement st = con.createStatement();
 					ResultSet rs= st.executeQuery(query);
-					System.out.println("Escriba el nombre de id del pais que representa el jugador: ");
+					System.out.println("Elija pais:");
 					while (rs.next()){
 						System.out.println(rs.getInt("idpais")+" : "+rs.getString("nombre"));
 					}
-					int paisID = sc.nextInt();
-					rs= st.executeQuery("SELECT nombre FROM pais WHERE idpais = "+paisID);
-					System.out.println("¿Quiere agregar a "+nombre+" "+apellido+" a "+rs.getString("pais")+"? SI / NO");
-					String opcion = sc.nextLine();
-					boolean valido = false;
-					while (!valido) {
-						if (opcion.equals("SI")) {
-							valido = true;
-							// INSERTAR DATOS
-							st.executeUpdate("INSERT INTO futbolista (nombre,apellido,docIdentidad,telefono,mail,idpais) VALUES('"+nombre+"','"+apellido+"','"+docIdentidad+"','"+telefono+"','"+mail+"','"+paisID+"')");
-							System.out.println("Agregado exitosamente");
-						} else {
-							if (opcion.equals("NO")) {
-								valido = true;
-							} 
-							else {
-								System.out.println("Invalido");
-								System.out.println("¿Quiere agregar a "+nombre+" "+apellido+" en "+rs.getString("pais")+"? SI / NO");
-								opcion = sc.nextLine();
-							}
-						}
+					int paisID = input.nextInt();
+					input.nextLine(); 
+					query = "SELECT * FROM pais WHERE idpais = ?";
+					PreparedStatement pst = con.prepareStatement(query);
+					pst.clearParameters();
+					pst.setInt(1,paisID);
+					ResultSet rs2 = pst.executeQuery();
+					rs2.next();
+					System.out.println("¿Desea agregar a "+nombre+" "+apellido+" a "+rs2.getString("nombre")+"? SI / NO");
+					String opcion = input.nextLine();
+					
+					while (!opcion.equals("SI") && !opcion.equals("NO")) {
+						System.out.println("Opcion invalida, intente de nuevo");
+						opcion = input.nextLine();
 					}
+						 if (opcion.equals("SI")) {
+							 pst = con.prepareStatement("INSERT INTO futbolista (nombre,apellido,docIdentidad,telefono,email,idpais) VALUES(?,?,?,?,?,?)");
+							 pst.setString(1,nombre);
+							 pst.setString(2,apellido);
+							 pst.setInt(3,docIdentidad);
+							 pst.setInt(4,telefono);
+							 pst.setString(5, mail);
+							 pst.setInt(6,paisID );
+							 pst.executeUpdate();
+							 pst.close();
+							 System.out.println("Agregado exitosamente");
+						 }
+					rs2.close();
 					rs.close();
 					st.close();
-					} catch (java.sql.SQLException e) {
-						System.out.println("Error de SQL: "+e.getMessage());
-					}
+					pst.close();
+				 } catch (java.sql.SQLException e) {
+					System.out.println(e.getMessage());
+				 }
+		}
 	}
+
+private static Object BuscarFutbolista(String apellido, Connection con) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
 
 // Busca detro de la base de datos el pais que se pide.
 	public static ClaseSede BuscarSede(String nombre,Connection con) {
@@ -197,28 +222,41 @@ public class main {
 		
 //busca una sede en la base de datos, y en caso de no encontrarla, este lo agrega
 		public static void AgregarSede(Connection con){				
+			input.nextLine();
 			System.out.println("Ingresar sede: ");
 			String nombre = input.nextLine();
-			if (BuscarSede(nombre,con)!=null){
+			if (BuscarSede(nombre,con)==null){
 				try{
-					Statement st = con.createStatement();
 					System.out.println("Ingresar capacidad: ");
 					int capacidad = input.nextInt();
+					input.nextLine();
 					System.out.println("Ingresar pais: ");
 					String pais = input.nextLine();
-					ResultSet rs= st.executeQuery("SELECT * FROM pais WHERE nombre = "+pais);
-					int res=st.executeUpdate("INSERT INTO sede (nombre,capacidad,idpais) VALUES('"+nombre+"','"+capacidad+"','"+rs.getInt("idpais")+"')");
-					if (res==0) System.out.println("Agregado exitosamente");
-					else System.out.println("Ocurrio un error");
+					String query = "SELECT * FROM pais WHERE nombre = ?";
+					PreparedStatement st = con.prepareStatement(query);
+					st.clearParameters();
+					st.setString(1,pais);
+					ResultSet rs = st.executeQuery();
+					rs.next();
+					query = "INSERT INTO sede (nombre,capacidad,idpais) VALUES(?,?,?)";
+					st = con.prepareStatement(query);
+					st.clearParameters();
+					st.setString(1,nombre);
+					st.setInt(2,capacidad);
+					st.setInt(3,rs.getInt("idpais"));
+					st.executeUpdate();
 					st.close();
+					System.out.print("La sede fue agregada con exito");
 				 } catch (java.sql.SQLException e) {
-					System.out.println("Error de SQL: "+e.getMessage());
+					System.out.println(e.getMessage());
 				 }
-			else System.out.println("La sede ingresado ya existe.");	
+			}
+			else  System.out.println("La sede ingresado ya existe.");	
 		}
 		
 // busca una sede y la edita
 		public static void EditarSede(Connection con) {
+			input.nextLine();
 			System.out.println("Ingresar sede: ");
 			String nombre = input.nextLine();
 				try{
@@ -237,12 +275,13 @@ public class main {
 						st.close();
 					}
 				 } catch (java.sql.SQLException e) {
-					 System.out.println("Error de SQL: "+e.getMessage());
+					 System.out.println(e.getMessage());
 				 }
 		}
 // busca una sede y la borra
 		
 		public static void EliminarSede(Connection con) {
+			input.nextLine();
 			System.out.println("Ingresar sede: ");
 			String nombre = input.nextLine();
 				try{
@@ -254,10 +293,8 @@ public class main {
 					}
 					else System.out.println("La sede ingresada no existe.");
 				 } catch (java.sql.SQLException e) {
-					 System.out.println("Error de SQL: "+e.getMessage());
+					 System.out.println(e.getMessage());
 				 }
 		}
-		
-		
 		
 }
